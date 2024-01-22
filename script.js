2c08769dcaf214e13f6ed592f03490a28d64f1b8
lage356 <fernando.lage356@gmail.com>
2024-01-22 10:50:37 -0800

    Updating styling and javascript code to show/remove sections of my code

--- script.js
+++ script.js
@@ -6,34 +6,17 @@ var quizWelcome = document.querySelector(".quiz-welcome");
 var gameTitle = document.querySelector(".titleW");
 var gameInstru = document.querySelector(".quiz-instr");
 var gameContainer = document.querySelector(".game-container");
+var registerPlayer = document.querySelector(".registerPlayer");
 var quizAnswers = document.getElementById("quiz-answers");
 var startButton = document.querySelector(".startButton");
 var displayRetro = document.querySelector(".renderValid");
 
 //? Here a declare variables
 var secondsLeft = 100;
-
 var finish = "All Done!";
 let currentQuestionIndex = 0;
 let score = 0;
 
-// var questionOne = "Commonly used data types DO NOT iclude:";
-// var questionTwo =
-//   "The condition in an if/else statement is enclosed within _______.";
-// var questionThree = "Arrays in JavaScript can be used to store ___________.";
-// var questionFour =
-//   "String values must be enclosed within ________ when being assigned to variables";
-// var questionFive =
-//   "A very useful tool used during development and debugging for printing content to the debugger is: ";
-// var answersOne = ["Strings", "Booleans", "Alerts", "Numbers"];
-// var answersTwo = ["Quotes", "Curly-brackets", "Parentheses", "Square-brackets"];
-// var asnwersThree = [
-//   "Numbers and strings",
-//   "Other arrays",
-//   "Booleans",
-//   "All of the above",
-// ];
-// var answersFour = ["Commas", "Curly brackets", "Quotes", "Parentheses"];
 
 const quizObject = [
   {
@@ -72,13 +55,15 @@ const quizObject = [
 ];
 
 function startGame() {
-  currentQuestion =0; 
-  currentQuestionIndex = 0
+  // currentQuestion =0; 
+  // currentQuestionIndex = 0
   secondsLeft = 100;
-  clearInterval();
+  // clearInterval();
   setTime();
-  hideWelcome();
-  unhideQuizSection();
+  // hideWelcome();
+  quizWelcome.classList.add('hide')
+  gameContainer.classList.remove('hide')
+  // unhideQuizSection();
   renderQuestions();
 }
 
@@ -88,13 +73,6 @@ function init() {
     "Try to answer the following code-related questions within the time limit. Keep inmind that incorrect answers will penalize your scoretime by 10 seconds!";
   gameTitle.textContent = welcomeH;
   gameInstru.textContent = instructionsP;
-}
-
-function hideWelcome() { 
-    quizWelcome.style.display= "none";
-}
-function showWelcome() { 
-    quizWelcome.style.display= "contents";
 }
 
 init();
@@ -113,6 +91,8 @@ function renderQuestions() {
   });
 }
 
+
+
 function selectOption(answersIndex) {
     
   const selectedOption = quizObject[currentQuestionIndex].answers[answersIndex];
@@ -120,30 +100,28 @@ function selectOption(answersIndex) {
   if (selectedOption === quizObject[currentQuestionIndex].correctAnswer) {
     score++;
     displayRetro.textContent = "Correct!";
+    
   } else {
     secondsLeft = secondsLeft - 10;
     displayRetro.textContent = "Incorrect!";
   }
+  
   currentQuestionIndex++;
+  
 
   if (currentQuestionIndex < quizObject.length) {
     renderQuestions();
   } else {
-    showWelcome();
-    hideQuizSection();
+   
+
+    gameContainer.classList.add('hide');
+    registerPlayer.classList.remove('hide');
     secondsLeft = 1;
-    
-//    currentQuestionIndex=0;
+
   }
   
 }
 
-function hideQuizSection() {
-    gameContainer.style.display= "none";
-}
-function unhideQuizSection() {
-    gameContainer.style.display= "contents";
-}
 
 function setTime() {
   var timeInterval = setInterval(function () {
